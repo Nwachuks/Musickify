6ddbe21bd9105934c45bb8234eb9f53ae1bdf4fd
Nwachukwu <nwachukwu@cowrywise.com>
2021-07-19 19:48:41 +0100

    Get auth code from Spotify

--- Musickify/Controllers/WelcomeVC.swift
+++ Musickify/Controllers/WelcomeVC.swift
@@ -8,23 +8,42 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
+    
+    private let signInBtn: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Sign In with Spotify", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         title = "Musickify"
         view.backgroundColor = .systemGreen
+        // Do any additional setup after loading the view.
+        view.addSubview(signInBtn)
+        signInBtn.addTarget(self, action: #selector(signInTapped), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        signInBtn.frame = CGRect(x: 20, y: view.height-60-view.safeAreaInsets.bottom, width: view.width-40, height: 50)
     }
-    */
+    
+    @objc func signInTapped() {
+        let vc = LoginVC()
+        vc.completionHandler = { [weak self] success in
+            DispatchQueue.main.async {
+                self?.handleSignIn(success: success)
+            }
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func handleSignIn(success: Bool) {
+        // Log in user or notify for error
+    }
 
 }
--- Musickify/Managers/AuthManager.swift
+++ Musickify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let instance = AuthManager()
     
+    struct Constants {
+        static let clientID = "bf6f4b88a21b4044955fe7011aa4e27b"
+        static let clientSecret = "6bd76173181243ed921c6284f87d6688"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let baseURL = "https://accounts.spotify.com/authorize"
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let fullURL = "\(baseURL)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: fullURL)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +43,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(string: String, completion: @escaping (Bool) -> Void) {
+        // Get Token
+    }
+    
+    private func cacheToken() {
+        
+    }
+    
+    public func refreshAccessToken() {
+        
     }
 }
